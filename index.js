9e2ef47898c5508e0939d47a2b6974175f9d94c1
Kira Sl <42243887+KiraSl@users.noreply.github.com>
2019-07-28 11:02:39 +0100

    Add endpoints details to the documentation.html

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const app = express();
 
 let oscarMovies = [ {
   title: 'Green Book',
-  dateOfIssue: '2018',
-  director: 'Peter Farrelly',
-  genre: [
+  dateOfIssue: 2018,
+  directors: [
+    {
+      name: 'Peter Farrelly',
+      bio: '',
+      birthYear: 1956,
+      deathYear: null,
+    }
+  ],
+  genres: [
     'Biography', 
     'Comedy', 
     'Drama'
@@ -15,9 +22,16 @@ let oscarMovies = [ {
 },
 {
   title: 'The Shape of Water',
-  dateOfIssue: '2017',
-  director: 'Guillermo del Toro',
-  genre: [
+  dateOfIssue: 2017,
+  directors: [
+    {
+      name: 'Guillermo del Toro',
+      bio: '',
+      birthYear: 1964,
+      deathYear: null,
+    }
+  ],
+  genres: [
     'Adventure', 
     'Drama', 
     'Fantasy'
@@ -26,18 +40,32 @@ let oscarMovies = [ {
 },
 {
   title: 'Moonlight',
-  dateOfIssue: '2016',
-  director: 'Barry Jenkins',
-  genre: [
+  dateOfIssue: 2016,
+  directors: [
+    {
+      name: 'Barry Jenkins',
+      bio: '',
+      birthYear: 1979,
+      deathYear: null,
+    }
+  ],
+  genres: [
     'Drama'
     ],
   description: 'A young African-American man grapples with his identity and sexuality while experiencing the everyday struggles of childhood, adolescence, and burgeoning adulthood.'
 },
 {
   title: 'Spotlight',
-  dateOfIssue: '2015',
-  director: 'Tom McCarthy',
-  genre: [
+  dateOfIssue: 2015,
+  directors: [
+    {
+      name: 'Tom McCarthy',
+      bio: '',
+      birthYear: 1966,
+      deathYear: null,
+    }
+  ],
+  genres: [
     'Biography', 
     'Crime', 
     'Drama'
@@ -46,9 +74,16 @@ let oscarMovies = [ {
 },
 {
   title: 'Birdman',
-  dateOfIssue: '2014',
-  director: 'Alejandro G. Iñárritu',
-  genre: [
+  dateOfIssue: 2014,
+  directors: [
+    {
+    name: 'Alejandro G. Iñárritu',
+    bio: '',
+    birthYear: 1963,
+    deathYear: null,
+    }
+  ],
+  genres: [
     'Comedy', 
     'Drama'
     ],
@@ -56,9 +91,16 @@ let oscarMovies = [ {
 },
 {
   title: '12 Years a Slave',
-  dateOfIssue: '2013', 
-  director: 'Steve McQueen',
-  genre: [
+  dateOfIssue: 2013, 
+  directors: [
+    {
+      name: 'Steve McQueen',
+      bio: '',
+      birthYear: 1969,
+      deathYear: null,
+    }
+  ],
+  genres: [
     'Biography', 
     'Drama', 
     'History'
@@ -67,9 +109,16 @@ let oscarMovies = [ {
 },
 {
   title: 'Argo',
-  dateOfIssue: '2012',
-  director: 'Ben Affleck',
-  genre: [
+  dateOfIssue: 2012,
+  directors: [
+    {
+      name: 'Ben Affleck',
+      bio: '',
+      birthYear: 1972,
+      deathYear: null,
+    }
+  ],
+  genres: [
     'Biography', 
     'Drama', 
     'Thriller'
@@ -78,9 +127,16 @@ let oscarMovies = [ {
 },
 {
   title: 'The Artist',
-  dateOfIssue: '2011',
-  director: 'Michel Hazanavicius',
-  genre: [
+  dateOfIssue: 2011,
+  directors: [
+    {
+      name: 'Michel Hazanavicius',
+      bio: '',
+      birthYear: 1967,
+      deathYear: null,
+    }
+  ],
+  genres: [
     'Comedy', 
     'Drama', 
     'Romance'
@@ -89,9 +145,16 @@ let oscarMovies = [ {
 },
 {
   title: 'The King\'s Speech',
-  dateOfIssue: '2010',
-  director: 'Tom Hooper',
-  genre: [
+  dateOfIssue: 2010,
+  directors: [
+    {
+      name: 'Tom Hooper',
+      bio: '',
+      birthYear: 1972,
+      deathYear: null,
+    }
+  ],
+  genres: [
     'Biography', 
     'Drama', 
     'History'
@@ -100,9 +163,22 @@ let oscarMovies = [ {
 },
 {
   title: 'Slumdog Millionaire',
-  dateOfIssue: '2008',
-  director: 'Danny Boyle, Loveleen Tandan',
-  genre: [
+  dateOfIssue: 2008,
+  directors: [
+    {
+      name: 'Danny Boyle',
+      bio: '',
+      birthYear: 1956,
+      deathYear: null,
+    }, 
+    { 
+      name: 'Loveleen Tandan',
+      bio: '',
+      birthYear: null,
+      deathYear: null,
+    }
+  ],
+  genres: [
     'Drama', 
     'Romance'
     ],
@@ -119,10 +195,44 @@ app.use(function (err, req, res, next) {
 
 //GET requests 
 app.get('/', function(req, res) {
-  res.send('Welcome!')
+  res.send('Welcome!');
 });
 app.get('/movies', function(req, res) {
-  res.json(oscarMovies)
+  res.json(oscarMovies);
+});
+app.get('/movies/:title', function(req, res) {
+  res.send('Successful GET request returning data about a movie by title');
+});
+app.get('/genres/:name', function(req, res) {
+  res.send('Successful GET request returning data about a particular genre');
+});
+app.get('/directors/:name', function(req, res) {
+  res.send('Successful GET request returning data about a director by name');
+});
+app.get('/account/:id/favorite/movies', function(req, res) {
+  res.send('Successful GET request returning data about favorite movies of a user');
+});
+
+//POST requests 
+app.post('/account', function(req, res) {
+  res.send('Successful POST request allowing to register a user');
+});
+app.post('/account/:id/favorite/movies', function(req, res) {
+  res.send('Successful POST request adding a movie to the user\'s list of favorite movies');
+});
+
+//PUT requests
+app.put('/account/:id', function(req, res) {
+  res.send('Successful PUT request allowing the user to update profile information');
+});
+
+//DELETE requests
+app.delete('/account/:id/favorite/movies/:title', function(req, res) {
+  res.send('Successful DELETE request removing a movie from the user\'s list of favorite movies');
+});
+
+app.delete('/account/:id', function(req, res) {
+  res.send('Successful DELETE request removing the user\'s account');
 });
 
 app.listen(8080);
