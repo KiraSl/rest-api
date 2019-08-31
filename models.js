eba5bf8e0c0bfb8332280685f9ce6e1702878a24
Kira Slobodina <slobodina.kira@gmail.com>
2019-08-31 15:05:24 +0100

    Extend REST API

--- models.js
+++ models.js
@@ -1,42 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //Define a schema for movies
 let movieSchema = mongoose.Schema({
-  Title: {type: String, required: true},
-  Description: {type: String, required: true}, 
-  Year: {type: String, required: true},
-  Genres: [ 
-    {
-      Title: {type: String, required: true},
-      Description: {type: String, required: true}
-    }
-  ],
-  Directors: [
-    {
-      Name: {type: String, required: true},
-      Born: {type: String, required: true},
-      Death: String,
-      Bio: String
-    }
-  ],
-  ImageURL: String, 
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Year: { type: String, required: true },
+  Genre: {
+    Title: { type: String, required: true },
+    Description: String
+  },
+  Director: {
+    Name: { type: String, required: true },
+    Born: { type: String, required: true },
+    Death: String,
+    Bio: String
+  },
+  ImageURL: String,
   Featured: Boolean
 });
 
 //Define a schema for users
 let userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
   Birthday: Date,
-  FavoriteMovies: [
-    {type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}
-  ]
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
 });
 
 //Create models that use schemas defined above
-let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User', userSchema);
+let Movie = mongoose.model("Movie", movieSchema);
+let User = mongoose.model("User", userSchema);
 
 //Export models in order to be able to import them into "index.js" file
 module.exports.Movie = Movie;
